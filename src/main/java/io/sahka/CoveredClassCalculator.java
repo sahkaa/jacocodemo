6b5b1d9671555c0cafa19b98bb0bdf43a1a07256
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-05-27 04:05:20 +0300

    - added not covered

--- src/main/java/io/sahka/CoveredClassCalculator.java
+++ src/main/java/io/sahka/CoveredClassCalculator.java
@@ -9,4 +9,12 @@ public class CoveredClassCalculator {
     public static int divide(int a, int b) {
         return a / b;
     }
+
+    public static int multipleNotCovered(int a, int b) {
+        return a * b;
+    }
+
+    public static int divideNotCovered(int a, int b) {
+        return a / b;
+    }
 }
