1ab9655eb8da1c0ae6c627927be3a613d712e0a9
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-05-28 18:29:02 +0200

    - removed

--- src/main/java/io/sahka/CoveredClassCalculator.java
+++ src/main/java/io/sahka/CoveredClassCalculator.java
@@ -10,35 +10,4 @@ public class CoveredClassCalculator {
         return a / b;
     }
 
-    public static int multipleNotCovered(int a, int b) {
-        return a * b;
-    }
-
-    public static int divideNotCovered(int a, int b) {
-        return a / b;
-    }
-
-    public static int multipleNotCovered3(int a, int b) {
-        return a * b;
-    }
-
-    public static int divideNotCovered3(int a, int b) {
-        return a / b;
-    }
-
-    public static int multipleNotCovered33(int a, int b) {
-        return a * b;
-    }
-
-    public static int divideNotCovered33(int a, int b) {
-        return a / b;
-    }
-
-    public static int multipleNotCovered333(int a, int b) {
-        return a * b;
-    }
-
-    public static int divideNotCovered333(int a, int b) {
-        return a / b;
-    }
 }
