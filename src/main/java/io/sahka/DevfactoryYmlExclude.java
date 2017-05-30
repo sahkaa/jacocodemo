9c17e3f397c0b1344516d47386462f49c3572867
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-05-30 03:43:48 +0200

    -devfactory added

---
+++ src/main/java/io/sahka/DevfactoryYmlExclude.java
@@ -0,0 +1,13 @@
+package io.sahka;
+
+public class DevfactoryYmlExclude {
+
+    public static int multiple(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+}
---
+++ src/test/java/io/sahka/DevfactoryYmlTest.java
@@ -0,0 +1,20 @@
+package io.sahka;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class DevfactoryYmlTest {
+
+    @Test
+    public void multiple() throws Exception {
+        assertThat(DevfactoryYmlExclude.multiple(2, 3), is(6));
+    }
+
+    @Test
+    public void divide() throws Exception {
+        assertThat(DevfactoryYmlExclude.divide(6, 3), is(2));
+    }
+
+}
