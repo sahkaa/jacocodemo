5ed8ec291e99c2c537441a807a4f923f46da6555
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-05-30 02:54:21 +0200

    -removed uncovered code

--- src/test/java/io/sahka/CoveredClassCalculatorTest.java
+++ src/test/java/io/sahka/CoveredClassCalculatorTest.java
@@ -1,10 +1,5 @@
 package io.sahka;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
-
-import org.junit.Test;
-
 public class CoveredClassCalculatorTest {
 
     public static int multipleNotCovered(int a, int b) {
@@ -13,15 +8,5 @@ public class CoveredClassCalculatorTest {
 
     public static int divideNotCovered(int a, int b) {
         return a / b;
-    }
-
-    @Test
-    public void multiple() throws Exception {
-        assertThat(CoveredClassCalculator.multiple(2, 3), is(6));
-    }
-
-    @Test
-    public void divide() throws Exception {
-        assertThat(CoveredClassCalculator.divide(6, 3), is(2));
     }
 }
