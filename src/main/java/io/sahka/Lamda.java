e1aff1d5338d456e5b0fcb2a6fd985e3137b7e3e
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-07-19 13:33:08 +0200

    Revert: - offline instrumentation with powermock  v2

---
+++ src/main/java/io/sahka/Lamda.java
@@ -0,0 +1,33 @@
+package io.sahka;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * @author Alexander Yushchenko
+ * @since 2017.04.15
+ */
+public class Lamda {
+
+    public static String capitalize(String in) {
+        String result = "";
+        String[] a = in.split(",");
+        for (String s : a) {
+            result += s.substring(0, 1).toUpperCase() + s.substring(1, s.length());
+        }
+        return result;
+    }
+
+    public void validate(List<String> in) {
+        doWithList(in, s -> {
+            if (s != null && s.length() > 10) {
+                throw new RuntimeException("");
+            }
+        });
+    }
+
+    private void doWithList(List<String> in, Consumer<String> consumer) {
+        in.stream().map(Lamda::capitalize).forEach(consumer);
+    }
+
+}
---
+++ src/main/java/io/sahka/MockedClass.java
@@ -0,0 +1,8 @@
+package io.sahka;
+
+public class MockedClass {
+
+    public String dummyMethod() {
+        return "dummyValue";
+    }
+}
