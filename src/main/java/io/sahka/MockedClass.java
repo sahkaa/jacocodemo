694cb7fe18113a8d7fb50aa9a5fd3c6bab5ed50e
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2017-08-30 23:41:42 +0200

    added lombok

--- src/main/java/io/sahka/MockedClass.java
+++ src/main/java/io/sahka/MockedClass.java
@@ -1,6 +1,13 @@
 package io.sahka;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MockedClass {
+
+    private String getOrSet;
 
     public String dummyMethod() {
         return "dummyValue";
