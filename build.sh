1cc475e504311d3d51dc2d195f97b082a097514e
sahkaa <c9c8b9adfa25ae0135cddd8f5e72005dd4592002>
2018-01-17 04:47:22 +0100

    * added build.sh

---
+++ build.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+set JAVA_HOME=/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+/Users/sahka/apache-maven-3.0.5/bin/mvn -version
+/Users/sahka/apache-maven-3.0.5/bin/mvn clean install
